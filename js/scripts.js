077faedf38d2c52daaebfb2ca9515061dd0f3b35
Natalia Tigh <natih6218@gmail.com>
2019-06-03 13:27:59 -0700

    add 3 forms for multiplication, division and substraction

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,48 @@
+
 var add = function(number1, number2){
-return number1 - number2;
+return number1 + number2;
 };
 
-var number1 = parseInt(prompt('Enter a number : '));
+var substract = function(number1, number2){
+  return number1 - number2;
+};
 
-var number2 = parseInt(prompt('Enter another number: '));
+var multiply = function(number1, number2){
+  return number1 * number2;
+};
+var divide = function(number1, number2){
+  return number1 / number2;
+};
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+  var number1 = parseInt($("#add1").val());
+  var number2 = parseInt($("#add2").val());
+  var result = add(number1, number2);
+  $("#output").text(result);
+  });
 
-alert(add(number1, number2));
+  $("form#substract").submit(function(event) {
+    event.preventDefault();
+  var number1 = parseInt($("#less1").val());
+  var number2 = parseInt($("#less2").val());
+  var substract = parseInt(number1) - parseInt(number2);
+  $("#output").text(substract);
+  });
+
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+  var number1 = parseInt($("#multiply1").val());
+  var number2 = parseInt($("#multiply2").val());
+  var multiply = parseInt(number1) * parseInt(number2);
+  $("#output").text(multiply);
+  });
+
+  $("form#divide").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var divide = (number1) / (number2);
+    $("#output").text(divide);
+  });
+});
