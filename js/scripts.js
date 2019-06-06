47087f9dc04f3ce389127ecde5f2d9f75053f6e5
Student <example@example.com>
2019-06-05 18:34:21 -0700

    add radio button" "

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ var add = function(number1, number2){
 return number1 + number2;
 };
 
-var substract = function(number1, number2){
+var subtract = function(number1, number2){
   return number1 - number2;
 };
 
@@ -14,35 +14,21 @@ var divide = function(number1, number2){
   return number1 / number2;
 };
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#calculator").submit(function(event) {
     event.preventDefault();
-  var number1 = parseInt($("#add1").val());
-  var number2 = parseInt($("#add2").val());
-  var result = add(number1, number2);
-  $("#output").text(result);
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
+    });
   });
-
-  $("form#substract").submit(function(event) {
-    event.preventDefault();
-  var number1 = parseInt($("#less1").val());
-  var number2 = parseInt($("#less2").val());
-  var substract = parseInt(number1) - parseInt(number2);
-  $("#output").text(substract);
-  });
-
-  $("form#multiply").submit(function(event) {
-    event.preventDefault();
-  var number1 = parseInt($("#multiply1").val());
-  var number2 = parseInt($("#multiply2").val());
-  var multiply = parseInt(number1) * parseInt(number2);
-  $("#output").text(multiply);
-  });
-
-  $("form#divide").submit(function(event){
-    event.preventDefault();
-    var number1 = parseInt($("#divide1").val());
-    var number2 = parseInt($("#divide2").val());
-    var divide = (number1) / (number2);
-    $("#output").text(divide);
-  });
-});
